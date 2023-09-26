6c70f37a27b763d288fef26f1d068cc09c80b1e1
saeyeun <uysy1200@gmail.com>
2023-09-27 04:45:18 +0900

    swiper slide 적용

--- js/script.js
+++ js/script.js
@@ -1,44 +1,44 @@
 $(document).ready(function () {
-  $('.mainSlide').slick({
-    dots: true,
-    arrows: false,
-    infinite: true,
-    speed: 1000,
-    slidesToShow: 1,
-    centerMode: true,
-    variableWidth: true,
-    pauseOnHover : false,
-    autoplay: true,
-    autoplaySpeed: 1000,
-    draggable: false,
-    responsive: [
-            {
-              breakpoint: 733,
-              settings: {
-                arrows: true,
-                infinite: true,
-                dots: true
-              }
-            },
-            // {
-            //   breakpoint: 600,
-            //   settings: {
-            //     slidesToShow: 2,
-            //     slidesToScroll: 2
-            //   }
-            // },
-            // {
-            //   breakpoint: 480,
-            //   settings: {
-            //     slidesToShow: 1,
-            //     slidesToScroll: 1
-            //   }
-            // }
-            // You can unslick at a given breakpoint now by adding:
-            // settings: "unslick"
-            // instead of a settings object
-          ]
-  });
+  // $('.mainSlide').slick({
+  //   dots: true,
+  //   arrows: false,
+  //   infinite: true,
+  //   speed: 1000,
+  //   slidesToShow: 1,
+  //   centerMode: true,
+  //   variableWidth: true,
+  //   pauseOnHover : false,
+  //   autoplay: true,
+  //   autoplaySpeed: 1000,
+  //   draggable: false,
+  //   responsive: [
+  //           {
+  //             breakpoint: 733,
+  //             settings: {
+  //               arrows: true,
+  //               infinite: true,
+  //               dots: true
+  //             }
+  //           },
+  //           // {
+  //           //   breakpoint: 600,
+  //           //   settings: {
+  //           //     slidesToShow: 2,
+  //           //     slidesToScroll: 2
+  //           //   }
+  //           // },
+  //           // {
+  //           //   breakpoint: 480,
+  //           //   settings: {
+  //           //     slidesToShow: 1,
+  //           //     slidesToScroll: 1
+  //           //   }
+  //           // }
+  //           // You can unslick at a given breakpoint now by adding:
+  //           // settings: "unslick"
+  //           // instead of a settings object
+  //         ]
+  // });
 
   // $('.mobile-menu').click(function (e) {
   //   e.preventDefault();
